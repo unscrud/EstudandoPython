1c572adea7f2c7d9f82cfb793ab6c60fe4bbf096
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-08 19:05:08 -0300

    Commit inicial do estudo de python.

---
+++ capitulo1/abertura.py
@@ -0,0 +1,16 @@
+# primeiro arquivo criado com o unico objetivo
+# de fazer o velho print de olá mundo :)
+
+print("olá mundo")
+
+'''
+Repare que não há necessidade de ponto e vírgula 
+e nem de uma estrutura externa como uma classe ou
+método principal de execução como no java.
+
+Repare também que o comentário de uma linha usa tralha (Jogo da velha)
+e comentário de multiplas linhas está entre triplas aspas simples
+
+para efeito comparativo em java utilizamos // para comentário de uma linha
+e /* ... */ para comentários de multiplas linhas
+'''
