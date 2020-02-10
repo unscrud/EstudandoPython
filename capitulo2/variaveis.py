c796be2940ccd97471e2304f65f29cb44bc1ea4f
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-09 21:19:44 -0300

    Iniciando o capítulo 2

---
+++ capitulo2/variaveis.py
@@ -0,0 +1,15 @@
+# Eu não posso fazer uma equação sem as variáveis.
+# Tony Stark
+
+# Exemplos de atribuição de valores à variáveis
+message = 'And now for something completely different'
+n = 17
+pi = 3.141592653589793
+
+'''
+Observações:
+ - não pode começar com número
+ - nomes de variáveis com muitas palavras usam o _ como separador (snake case)
+ - alguns caracteres não podem ser usados em variáveis como o @ por exemplo
+ - palavras-chave como class ou if ou for não podem ser usadas como nome de variável
+'''
