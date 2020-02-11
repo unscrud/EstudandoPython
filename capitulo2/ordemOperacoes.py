1e7635eabbfd1f2608f3dde86cc5d8f6d2fe10f2
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-10 22:12:01 -0300

    Precedência das expressões matemáticas

---
+++ capitulo2/ordemOperacoes.py
@@ -0,0 +1,9 @@
+# A ordem é o prazer da razão: mas a desordem é a delícia da imaginação.
+# Paul Claudel
+
+print("Assim como nas expressões matemáticas, existe uma ordem de prioridade nas expreções executadas no python.")
+print("PEMDAS é uma acrônimo que ajuda a lembrar dessa ordem.")
+print("P - parenteses, mais alta precedência.")
+print("E - exponenciação vem em seguida se não houver parenteses")
+print("MD - Multiplicação e divisão são os próximos")
+print("AS - Adição e subtração são os últimos executados ")
