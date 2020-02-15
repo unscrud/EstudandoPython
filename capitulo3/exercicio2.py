e837f8f9795d57ff71b42c2dd63b5b36e29f0257
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-15 17:32:52 -0300

    Exercício 2 finalizado

--- capitulo3/exercicio2.py
+++ capitulo3/exercicio2.py
@@ -7,4 +7,18 @@ def print_algo(algo):
     print(algo)
 
 
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+
+def do_four(f, v):
+    do_twice(f, v)
+    do_twice(f, v)
+
+
 do_twice(print_algo, 'UnsCrud')
+
+do_twice(print_twice, "Teste")
+
+do_four(print_algo, "Quatro")
