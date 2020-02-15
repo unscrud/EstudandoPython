66d49a20b2e123dd4c750605497e5bbc6f03a741
Daniel Carvalho <daniel.devop@gmail.com>
2020-02-15 18:25:57 -0300

    fim do terceiro capítulo

---
+++ capitulo3/exercicio3.py
@@ -0,0 +1,16 @@
+def print_linha():
+    print("+", "-"*4, "+", "-"*4, "+")
+
+
+def print_pipes():
+    print("|", " "*4, "|", " "*4, "|")
+    print("|", " "*4, "|", " "*4, "|")
+    print("|", " "*4, "|", " "*4, "|")
+    print("|", " "*4, "|", " "*4, "|")
+
+
+print_linha()
+print_pipes()
+print_linha()
+print_pipes()
+print_linha()
