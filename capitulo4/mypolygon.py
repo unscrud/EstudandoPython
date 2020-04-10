4b52c8210f6803fb053f7f8a4a820aa9e8ff4fcc
Daniel Carvalho <daniel.devop@gmail.com>
2020-04-10 18:55:39 -0300

    adicionando a função polygon

--- capitulo4/mypolygon.py
+++ capitulo4/mypolygon.py
@@ -2,12 +2,21 @@ import turtle
 bob = turtle.Turtle()
 
 
-def square(t):
+def square(t, length):
     for i in range(4):
-        t.fd(150)
+        t.fd(length)
         t.lt(90)
     print(t)
     turtle.mainloop()
 
 
-square(bob)
+def polygon(t, length, n):
+    for i in range(n):
+        t.fd(length)
+        t.lt(360/n)
+    print(t)
+    turtle.mainloop()
+
+
+#square(bob, 200)
+polygon(bob, 50, 7)
