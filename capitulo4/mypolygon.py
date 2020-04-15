f7764e5b3cb6e2e8ed154168468102deb107fd6d
Daniel Carvalho <daniel.devop@gmail.com>
2020-04-15 19:24:48 -0300

    finalizando exercícios do tópico 4.3

--- capitulo4/mypolygon.py
+++ capitulo4/mypolygon.py
@@ -18,5 +18,23 @@ def polygon(t, length, n):
     turtle.mainloop()
 
 
+def circle(t, r):
+    n = 100
+    length = (2*3.14*r)/n
+    polygon(t, length, n)
+
+
+def arc(angle, t, r):
+    n = 100
+    length = (2*3.14*r)/n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle/n)
+    print(t)
+    turtle.mainloop()
+
+
 #square(bob, 200)
-polygon(bob, 50, 7)
+#polygon(bob, 50, 7)
+#circle(bob, 50)
+arc(30, bob, 25)
